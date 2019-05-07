c7f490c7caf92df88eac418e4e9605b9a5d00e2d
UENISHI Kota <kuenishi@gmail.com>
2019-05-07 13:32:00 +0900

    0.0.3 Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="machi-store",
-    version="0.0.2",
+    version="0.0.3",
     description="Persistent Blob Store",
     author="Kota UENISHI",
     author_email="kuenishi@gmail.com",
