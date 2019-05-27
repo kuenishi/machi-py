2b5d3453d004416fea84652e97b6d996b31f9e8b
UENISHI Kota <kota@preferred.jp>
2019-05-27 15:21:57 +0900

    Bump version
    Signed-off-by: UENISHI Kota <kota@preferred.jp>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="machi-store",
-    version="0.0.3",
+    version="0.0.4",
     description="Persistent Blob Store",
     author="Kota UENISHI",
     author_email="kuenishi@gmail.com",
