30f09eb148173e3810dce1ebb5d94be0c43fb3fe
UENISHI Kota <kota@preferred.jp>
2019-05-27 17:52:38 +0900

    Relax Python version and bump version to release

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="machi-store",
-    version="0.0.4",
+    version="0.0.5",
     description="Persistent Blob Store",
     author="Kota UENISHI",
     author_email="kuenishi@gmail.com",
     url="https://github.com/kuenishi/machi-py/",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    python_requires=">=3.7",
+    python_requires=">=3.5",
     extras_require={"test": ["coverage", "black", "pytest", "pytest-xdist", "numpy"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -16,6 +16,8 @@ setup(
         #   5 - Production/Stable
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
 )
